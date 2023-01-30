595c75ee5162b3130d37942dfe197ff870aa754a
Mauricio Rojas <mrojase@vitamina.com>
2023-01-30 14:43:01 -0300

    chore: added use case account

--- infrastructure/entry-points/api-rest/src/main/java/co/com/bancolombia/api/ApiRest.java
+++ infrastructure/entry-points/api-rest/src/main/java/co/com/bancolombia/api/ApiRest.java
@@ -1,20 +1,32 @@
 package co.com.bancolombia.api;
+import co.com.bancolombia.model.account.Account;
+import co.com.bancolombia.usecase.account.AccountUseCase;
 import lombok.AllArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "/api", produces = MediaType.APPLICATION_JSON_VALUE)
 @AllArgsConstructor
 public class ApiRest {
-//    private final MyUseCase useCase;
+    private final AccountUseCase accountUseCase;
 
+    @GetMapping(path = "/health")
+    public String health(){
+        return "up";
+    }
 
-    @GetMapping(path = "/path")
-    public String commandName() {
-//      return useCase.doAction();
-        return "Hello World";
+    @GetMapping(path = "/accounts")
+    public List<Account> getAccounts() {
+        return accountUseCase.getAccounts();
+    }
+
+    @GetMapping(path = "/account/:id")
+    public Account getAccounts(int accountNumber) {
+        return accountUseCase.getAccountById(accountNumber);
     }
 }
